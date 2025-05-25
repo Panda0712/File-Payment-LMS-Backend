546d3f3d52e353f3ca14ae5e76bee9f5c17c0eaa
Pham Tuan <tuan.pham.super@gmail.com>
2025-05-25 19:17:27 +0700

    try to fix large video uploading

--- src/providers/CloudinaryProvider.js
+++ src/providers/CloudinaryProvider.js
@@ -1,5 +1,8 @@
 import cloudinary from "cloudinary";
 import streamifier from "streamifier";
+import fs from "fs";
+import os from "os";
+import path from "path";
 import { env } from "~/config/environment";
 
 const cloudinaryV2 = cloudinary.v2;
@@ -23,27 +26,53 @@ const streamUpload = (fileBuffer, folderName) => {
   });
 };
 
-const streamUploadVideo = (fileBuffer, folderName, options = {}) => {
+// const streamUploadVideo = (fileBuffer, folderName, options = {}) => {
+//   return new Promise((resolve, reject) => {
+//     const uploadOptions = {
+//       folder: folderName,
+//       resource_type: "video",
+//       ...options,
+//     };
+
+//     const stream = cloudinaryV2.uploader.upload_stream(
+//       uploadOptions,
+//       (err, result) => {
+//         if (err) reject(err);
+//         else resolve(result);
+//       }
+//     );
+
+//     streamifier.createReadStream(fileBuffer).pipe(stream);
+//   });
+// };
+
+const uploadLargeVideo = (fileBuffer, folderName, options = {}) => {
   return new Promise((resolve, reject) => {
-    const uploadOptions = {
-      folder: folderName,
-      resource_type: "video",
-      ...options,
-    };
+    const tempFilePath = path.join(os.tmpdir(), `video-${Date.now()}.mp4`);
 
-    const stream = cloudinaryV2.uploader.upload_stream(
-      uploadOptions,
-      (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
+    // Ghi file tạm ra ổ đĩa
+    fs.writeFile(tempFilePath, fileBuffer, async (err) => {
+      if (err) return reject(err);
+
+      try {
+        const result = await cloudinaryV2.uploader.upload_large(tempFilePath, {
+          resource_type: "video",
+          folder: folderName,
+          chunk_size: 6 * 1024 * 1024, // 6MB mỗi chunk, có thể tăng
+          ...options,
+        });
+
+        fs.unlink(tempFilePath, () => {}); // Xoá file tạm sau upload
+        resolve(result);
+      } catch (error) {
+        fs.unlink(tempFilePath, () => {});
+        reject(error);
       }
-    );
-
-    streamifier.createReadStream(fileBuffer).pipe(stream);
+    });
   });
 };
 
 export const CloudinaryProvider = {
   streamUpload,
-  streamUploadVideo,
+  streamUploadVideo: uploadLargeVideo,
 };
